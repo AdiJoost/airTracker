6316643186b1cd4f5064201250794fa408316e90
AdiJoost <adi.joost@gmail.com>
2022-09-16 19:20:49 +0200

    logger added with csv option

--- src/tester.py
+++ src/tester.py
@@ -1,8 +1,12 @@
+from asyncio.log import logger
 from gpio_handler import GPIO_Handler
+from log.logger import Logger
 
 def main():
     gpio_handler = GPIO_Handler()
-    print(f"Temp: {gpio_handler.get_temperature()} | Humidity: {gpio_handler.get_humidity()}")
+    temp = gpio_handler.get_temperature()
+    humidity = gpio_handler.get_humidity()
+    Logger.log_csv(({temp},{humidity}))
 
 
 if __name__ == "__main__":
