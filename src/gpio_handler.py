da11c62df8d6d2971a3e4f203e0aa8e2cefe63d0
Adi Joost <adi.joost@gmail.com>
2022-09-17 23:10:39 +0200

    typo fixed, added requirements

--- src/gpio_handler.py
+++ src/gpio_handler.py
@@ -1,8 +1,8 @@
-#import Adafruit_DHT
+#import adafruit_dht
 
 class GPIO_Handler():
     def __init__(self):
-        #self.DHT_SENSOR = Adafruit_DHT.DHT11
+        #self.DHT_SENSOR = adafruit_dht.DHT11
         self.gas_sensor_analog = 14
         self.gas_sensor_digital = 15
         self.DHT_PIN = 4
@@ -13,11 +13,11 @@ class GPIO_Handler():
         pass
 
     def get_humidity(self):
-        #humidity, _ = Adafruit_DHT.read(self.DHT_SENSOR, self.DHT_PIN)
+        #humidity, _ = adafruit_dht.read(self.DHT_SENSOR, self.DHT_PIN)
         #return humidity
         return 70
 
     def get_temperature(self):
-        #_, temperature = Adafruit_DHT.read(self.DHT_SENSOR, self.DHT_PIN)
+        #_, temperature = adafruit_dht.read(self.DHT_SENSOR, self.DHT_PIN)
         #return temperature
         return 25
