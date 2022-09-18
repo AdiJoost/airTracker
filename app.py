1e48f75288513ab9f0650e42956a2f4a511608e7
Adi Joost <adi.joost@gmail.com>
2022-09-18 15:36:48 +0200

    trty to fix daemon shutdown

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 #from flask_restful import Api
 from flask_cors import CORS
 from db import db
-from src.log.logger import Logger
+from log.logger import Logger
+from src.Board_Controller import Board_Controller
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "superKevin"
+board_controller = Board_Controller()
 #api = Api(app)
 
 @app.before_first_request
@@ -20,6 +22,11 @@ def create_table():
 def home():
     return render_template("index.html")
 
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    board_controller.stop_daemon_thread()
+    return "daemon is dead"
+
 
 
 if __name__ == "__main__":
@@ -28,5 +35,8 @@ if __name__ == "__main__":
     #setup DB
     db.init_app(app)
     
+    #start daemon_thread
+    board_controller.start_daemon_thread()
+
     #start app
     app.run(port=5000, host="0.0.0.0", debug=True)
